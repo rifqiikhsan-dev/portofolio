ed1326143dcd07de5510ed1fd0f2a464d75b8614
praditia12 <aditpraditia714@gmail.com>
2025-04-08 01:24:33 +0700

    initial commit: project folder

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600 mb-4">
+        Tailwind CSS is Working! 🚀
+      </h1>
+      <p className="text-lg text-gray-700">
+        Edit <code>App.jsx</code> and save to test HMR.
+      </p>
+    </div>
+    </>
+  )
+}
+
+export default App
